1ae28f57778196e49f8599e8ad0113bf209a30bc
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:53:43 -0600

    Create hw_17.java

---
+++ hw_17.java
@@ -0,0 +1,15 @@
+public class hw_17 {
+	public static void main(String args[]){
+		int resul;
+		resul = sumar(350,250);
+		imprimir(resul);
+	}
+	public static int sumar(int a, int b){
+		int sumar=a+b;
+		return sumar;
+	}
+	public static void imprimir(int x){
+		System.out.println("La suma es: "+x);
+	}
+		
+	}
