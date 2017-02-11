bc5508837759de08621d6c786dc016fd3a8c7d11
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 16:03:48 -0600

    Create hw_22.java

---
+++ hw_22.java
@@ -0,0 +1,16 @@
+
+public class hw_22 {
+public static void main(String[]args){
+	primerSms();
+	segundoSms();
+	tercerSms();
+}
+public static void primerSms(){
+	System.out.println("Hola mundo :V");
+}
+public static void segundoSms(){
+	System.out.println("Sos un crack :V");
+}
+public static void tercerSms(){
+	System.out.println("Te trollié :V");
+}}
