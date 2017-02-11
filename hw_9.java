af0bcc030890cde205afbe2cae3fab6278180a4b
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:59:54 -0600

    Create hw_9.java

---
+++ hw_9.java
@@ -0,0 +1,12 @@
+public class hw_9 {
+public static void main(String args []){
+	int resultado;
+	resultado=dividir();
+	System.out.println("El resultado es"+resultado);
+	
+}
+public static int dividir(){
+	int a=100, b=10;
+	int dividir= a/b;
+			return dividir;
+}}
