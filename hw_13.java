4a1efa95b631ca0880d0d8826f2c24ec78bea9c0
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:53:14 -0600

    Create hw_13.java

---
+++ hw_13.java
@@ -0,0 +1,12 @@
+public class hw_13 {
+	public static void main(String args[]){
+		int resultado;
+		resultado=restar(84,44);
+		System.out.println("La resta es: " +resultado);
+
+	}
+	public static int restar(int a, int b){
+		int multi= a-b;
+		return multi;
+	}
+}
