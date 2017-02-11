26f134da7830438d3d8547407fa5b9267426a7b7
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 16:01:17 -0600

    Create hw_6.java

---
+++ hw_6.java
@@ -0,0 +1,11 @@
+public class hw_6 {
+	public static void main(String args[]){
+		int resultado;
+		resultado= sumar();
+		System.out.println("El resultado es:" +resultado);
+}
+	public static int sumar(){
+		int x=10, y=20;
+		int suma = x+y;
+		return suma;
+	}}
