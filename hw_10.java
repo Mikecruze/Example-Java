5fb4bc3570e25a44da83a2eab95c394ed9cc9c68
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:50:00 -0600

    Create hw_10.java

---
+++ hw_10.java
@@ -0,0 +1,12 @@
+public class hw_10 {
+public static void main(String args[]){
+	int resultado;
+	resultado=perimetro();
+	System.out.println("El resultado es" +resultado);
+}
+public static int perimetro(){
+	int a=20, b=30, c=40;
+	int perimetro= a+b+c;
+			return perimetro;
+}
+}
