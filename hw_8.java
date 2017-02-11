e9cf8c7854eb1f24c0c1b41bb7992eefa6358287
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 16:00:22 -0600

    Create hw_8.java

---
+++ hw_8.java
@@ -0,0 +1,12 @@
+public class hw_8 {
+public static void main(String args []){
+	int resultado;
+	resultado=multiplicar();
+	System.out.println("El resultado es" + resultado);
+}
+public static int multiplicar(){
+	int a=10, b=10;
+	int multiplicar= a*b;
+			return multiplicar;
+}
+}
