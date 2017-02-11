7672f965955882a527a43a778cbd7caf4fe41b60
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:51:30 -0600

    Create hw_12.java

---
+++ hw_12.java
@@ -0,0 +1,13 @@
+
+public class hw_12 {
+	public static void main(String args[]){
+		int resultado;
+		resultado=sumar(36,4);
+		System.out.println("La suma es: " +resultado);
+
+	}
+	public static int sumar(int a, int b){
+		int suma= a+b;
+		return suma;
+	}
+}
