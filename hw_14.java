4998e969816c1890e806f481977e79c41e31fb12
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:55:29 -0600

    Create hw_14.java

---
+++ hw_14.java
@@ -0,0 +1,12 @@
+public class hw_14 {
+	public static void main(String args[]){
+		int resultado;
+		resultado=dividir(100,4);
+		System.out.println("La división es: " +resultado);
+
+	}
+	public static int dividir(int a, int b){
+		int div= a/b;
+		return div;
+	}
+}
