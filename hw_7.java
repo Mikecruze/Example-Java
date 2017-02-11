593cf77183ed07fd296012925589004e71c6c781
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 16:00:50 -0600

    Create hw_7.java

---
+++ hw_7.java
@@ -0,0 +1,12 @@
+public class hw_7 {
+public static void main(String args[]){
+	int resultado;
+	resultado=restar();
+	System.out.println("El resultado es" +resultado);
+}
+public static int restar(){
+	int a=10, b=80;
+	int resta= b-a;
+			return resta;
+}
+}
