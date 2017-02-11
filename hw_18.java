a68630620e57be31992b13bfe5958e87ace73ec0
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:56:18 -0600

    Create hw_18.java

---
+++ hw_18.java
@@ -0,0 +1,15 @@
+public class hw_18 {
+	public static void main(String args[]){
+		int resul;
+		resul = restar(100,50);
+		imprimir(resul);
+	}
+	public static int restar(int a, int b){
+		int restar=a-b;
+		return restar;
+	}
+	public static void imprimir(int z){
+		System.out.println("La resta es: "+z);
+	}
+		
+	}
