243af8aaeed5608b2947629adf9c1151468119b6
Mikecruze <162t0307@itsm.edu.mx>
2017-02-11 15:52:20 -0600

    Create hw_11.java

---
+++ hw_11.java
@@ -0,0 +1,13 @@
+
+public class hw_11 {
+public static void main(String args[]){
+	int resultado;
+	resultado=multiplicar(3,5);
+	System.out.println("La multiplicación es: " +resultado);
+
+}
+public static int multiplicar(int a, int b){
+	int multi= a*b;
+	return multi;
+}
+}
